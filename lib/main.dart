a5b68d060f0c8a2636fc19edb1e402da45bdec56
lastoyster <kadampratiksha56@gmail.com>
2024-06-21 13:08:19 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: library_private_types_in_public_api, use_key_in_widget_constructors, prefer_typing_uninitialized_variables, depend_on_referenced_packages
+// Dòng chú thích trên đây được sử dụng để tắt đi các cảnh báo cụ thể trong mã nguồn.
+
+import 'package:flutter_student_database/database/database_local.dart'; // Import file liên quan đến cơ sở dữ liệu
+
+import 'package:flutter/material.dart'; // Import thư viện Flutter Material
+
+import 'pages/student/students_page.dart'; // Import trang Students
+
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); // Đảm bảo đã khởi tạo Flutter trước khi chạy ứng dụng
+  await DatabaseLocal.init(); // Khởi tạo cơ sở dữ liệu cục bộ
+  runApp(MyApp()); // Chạy ứng dụng chính
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: StudentList(), // Trang chính của ứng dụng là StudentList
+    );
+  }
+}
---
+++ lib/pages/course/courses_page.dart
@@ -0,0 +1,128 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first, library_private_types_in_public_api
+import 'package:flutter/material.dart';
+import 'package:flutter_student_database/database/database_local.dart';
+import 'package:flutter_student_database/models/course_model.dart';
+import 'package:flutter_student_database/models/student_model.dart';
+import 'package:flutter_student_database/pages/course/create_course_page.dart';
+import 'package:flutter_student_database/pages/course/edit_course_page.dart';
+
+class CoursesPage extends StatefulWidget {
+  final StudentModel student;
+
+  // Constructor cho trang danh sách khóa học, chấp nhận một đối tượng StudentModel.
+  const CoursesPage({super.key, required this.student});
+
+  @override
+  _CoursesPageState createState() => _CoursesPageState();
+}
+
+class _CoursesPageState extends State<CoursesPage> {
+  List<CourseModel> _courses = [];
+  double averageScore = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCourses();
+  }
+
+  _loadCourses() async {
+    // Tải danh sách các khóa học của sinh viên từ cơ sở dữ liệu.
+    List<CourseModel> courses =
+        await DatabaseLocal.getCoursesByStudent(widget.student.id ?? 0);
+    double totalScore = 0.0;
+
+    // Tính tổng số điểm của tất cả các khóa học.
+    for (var course in courses) {
+      totalScore += course.score;
+    }
+
+    setState(() {
+      _courses = courses;
+      averageScore = totalScore / courses.length;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Danh sách khóa học của ${widget.student.name}'),
+      ),
+      body: Column(
+        children: [
+          ListView.builder(
+            shrinkWrap: true,
+            itemCount: _courses.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(_courses[index].name),
+                subtitle: Text('Điểm: ${_courses[index].score}'),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () {
+                        // Chuyển đến trang chỉnh sửa khóa học.
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => EditCoursePage(
+                              course: _courses[index],
+                              student: widget.student,
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.delete),
+                      onPressed: () {
+                        // Xóa khóa học khỏi cơ sở dữ liệu và cập nhật danh sách.
+                        DatabaseLocal.deleteCourse(_courses[index].id ?? 0);
+
+                        _loadCourses();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+          if (_courses.isNotEmpty)
+            Text('Tổng điểm trung bình: ${averageScore.toStringAsFixed(2)}'),
+          if (_courses.isNotEmpty)
+            Text('Xếp loại: ${calculateGrade(averageScore)}'),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Chuyển đến trang thêm mới khóa học.
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => CreateCoursePage(
+                student: widget.student,
+              ),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  String calculateGrade(double averageScore) {
+    // Tính toán và trả về xếp loại dựa trên điểm trung bình.
+    if (averageScore >= 9.0) {
+      return "Xuất sắc";
+    } else if (averageScore >= 8.0) {
+      return "Giỏi";
+    } else if (averageScore >= 6.5) {
+      return "Khá";
+    } else if (averageScore >= 5.0) {
+      return "Trung bình";
+    } else {
+      return "Yếu";
+    }
+  }
+}
